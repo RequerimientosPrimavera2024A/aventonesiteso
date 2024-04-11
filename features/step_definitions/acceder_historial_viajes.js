cb02ba7a73158c5e792c782d4717b2c08abd57c3
MARIAN-SEDANO\Maria <mariansedanopaz@gmail.com>
2024-04-11 08:55:13 -0600

    add acceder_historial_viajes.js  file

---
+++ features/step_definitions/acceder_historial_viajes.js
@@ -0,0 +1,95 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+    Given('estoy en el perfil de un usuario', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+    When('intento ver la opción Historial de Viajes', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+    Then('se ve una lista con los viajes realizados por ese usuario', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+    Then('no se muestra la opción Historial de Viajes', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+
+    Given('estoy en la opción Historial de Viajes', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+
+    When('hago clic en Ver detalles del viaje', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+    Then('se me muestran los datos de la fecha, horas, rutas, compañeros de viaje, calificaciones y comentarios de todos los viajes realizados por ese usuario', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+    Given('estoy en la opción Historial de Viajes de manera intuitiva', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+    When('se me muestran todos los datos relevantes del viaje', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+    Then('se muestra el historial de viajes de manera intuitiva', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+
+    Given('estoy en la opción Historial de Viajes de otro usuario', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+    When('hago clic en Ir a mi perfil', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+    Then('me lleva a mi perfil', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+    Given('estoy en la opción Historial de Viajes de mi usuario', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+    When('hago click en editar y hago un cambio de un viaje programado', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+    Then('se actualiza correctamente el viaje programado', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+    When('hago click en editar y hago una adición de un viaje programado', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+    Then('se agrega la adición correctamente el viaje programado', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
