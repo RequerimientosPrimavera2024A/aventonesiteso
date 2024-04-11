89560a01cabe3baedbcddbbfc42d4a22711e9cf2
Luis Antonio Cuaquentzi Avendaño <luis.cuaquentzi@gmail.com>
2024-04-11 08:36:36 -0600

    Feature file and step definitions

---
+++ features/historial_de_viajes.feature
@@ -0,0 +1,47 @@
+Feature: Como usuario de Aventones ITESO, quiero poder acceder a mi historial de viajes para revisar detalles de viajes pasados y programados
+  La aplicación debe tener un historial de viajes que permita ver el detalle de cada uno
+
+  Scenario: En el que el usuario tiene viajes pasados que puede visualizar en el historial
+    Given el usuario ha realizado viajes pasados
+    When accede al historial de viajes en la aplicación
+    Then puede visualizar los viajes pasados en el historial
+
+  Scenario: En el que el usuario no tiene viajes pasados
+    Given el usuario no ha realizado viajes pasados
+    When accede al historial de viajes en la aplicación
+    Then no se muestra ningún viaje en el historial
+
+  Scenario: En el que el usuario tiene un historial de viajes
+    Given el usuario ha realizado varios viajes
+    When accede al historial de viajes en la aplicación
+    Then puede ver su historial completo de viajes
+
+  Scenario: En el que el usuario selecciona el viaje para conocer detalles
+    Given el usuario tiene un historial de viajes
+    When selecciona un viaje específico en el historial
+    Then puede ver los detalles completos de ese viaje
+
+  Scenario: En el que el usuario solo observa el resumen del viaje
+    Given el usuario tiene un historial de viajes
+    When observa el historial de viajes
+    Then puede ver un resumen de cada viaje, incluyendo la fecha, origen, destino y estado del viaje
+
+  Scenario: En el que la interfaz es intuitiva para el usuario
+    Given el usuario accede al historial de viajes en la aplicación
+    When interactúa con la interfaz del historial
+    Then encuentra fácilmente la información que busca y puede navegar sin dificultad
+
+  Scenario: En el que la interfaz es difícil de usar para el usuario
+    Given el usuario accede al historial de viajes en la aplicación
+    When intenta encontrar información en la interfaz del historial
+    Then encuentra dificultades para navegar y localizar la información deseada
+
+  Scenario: En el que ningún viaje debe ser actualizado
+    Given el usuario accede al historial de viajes en la aplicación
+    When revisa los viajes en el historial
+    Then ninguno de los viajes muestra actualizaciones o cambios
+
+  Scenario: En el que algún viaje debe ser actualizado en el historial
+    Given el usuario accede al historial de viajes en la aplicación
+    When revisa los viajes en el historial
+    Then encuentra al menos un viaje que ha sido actualizado con información nueva o modificada
---
+++ features/step_definitions/stepdefs.js
@@ -0,0 +1,200 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+// 1) Scenario: En el que el usuario tiene viajes pasados que puede visualizar en el historial # features\historial_de_viajes.feature:4
+//    ? Given el usuario ha realizado viajes pasados
+//        Undefined. Implement with the following snippet:
+
+         Given('el usuario ha realizado viajes pasados', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? When accede al historial de viajes en la aplicación
+//        Undefined. Implement with the following snippet:
+
+         When('accede al historial de viajes en la aplicación', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? Then puede visualizar los viajes pasados en el historial
+//        Undefined. Implement with the following snippet:
+
+         Then('puede visualizar los viajes pasados en el historial', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+// 2) Scenario: En el que el usuario no tiene viajes pasados # features\historial_de_viajes.feature:9
+//    ? Given el usuario no ha realizado viajes pasados
+//        Undefined. Implement with the following snippet:
+
+         Given('el usuario no ha realizado viajes pasados', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? When accede al historial de viajes en la aplicación
+//        Undefined. Implement with the following snippet:
+
+//    ? Then no se muestra ningún viaje en el historial
+//        Undefined. Implement with the following snippet:
+
+         Then('no se muestra ningún viaje en el historial', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+// 3) Scenario: En el que el usuario tiene un historial de viajes # features\historial_de_viajes.feature:14
+//    ? Given el usuario ha realizado varios viajes
+//        Undefined. Implement with the following snippet:
+
+         Given('el usuario ha realizado varios viajes', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? When accede al historial de viajes en la aplicación
+//        Undefined. Implement with the following snippet:
+
+//    ? Then puede ver su historial completo de viajes
+//        Undefined. Implement with the following snippet:
+
+         Then('puede ver su historial completo de viajes', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+// 4) Scenario: En el que el usuario selecciona el viaje para conocer detalles # features\historial_de_viajes.feature:19
+//    ? Given el usuario tiene un historial de viajes
+//        Undefined. Implement with the following snippet:
+
+         Given('el usuario tiene un historial de viajes', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? When selecciona un viaje específico en el historial
+//        Undefined. Implement with the following snippet:
+
+         When('selecciona un viaje específico en el historial', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? Then puede ver los detalles completos de ese viaje
+//        Undefined. Implement with the following snippet:
+
+         Then('puede ver los detalles completos de ese viaje', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+// 5) Scenario: En el que el usuario solo observa el resumen del viaje # features\historial_de_viajes.feature:24
+//    ? Given el usuario tiene un historial de viajes
+//        Undefined. Implement with the following snippet:
+
+//    ? When observa el historial de viajes
+//        Undefined. Implement with the following snippet:
+
+         When('observa el historial de viajes', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? Then puede ver un resumen de cada viaje, incluyendo la fecha, origen, destino y estado del viaje       
+//        Undefined. Implement with the following snippet:
+
+         Then('puede ver un resumen de cada viaje, incluyendo la fecha, origen, destino y estado del viaje', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+// 6) Scenario: En el que la interfaz es intuitiva para el usuario # features\historial_de_viajes.feature:29   
+//    ? Given el usuario accede al historial de viajes en la aplicación
+//        Undefined. Implement with the following snippet:
+
+         Given('el usuario accede al historial de viajes en la aplicación', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? When interactúa con la interfaz del historial
+//        Undefined. Implement with the following snippet:
+
+         When('interactúa con la interfaz del historial', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? Then encuentra fácilmente la información que busca y puede navegar sin dificultad
+//        Undefined. Implement with the following snippet:
+
+         Then('encuentra fácilmente la información que busca y puede navegar sin dificultad', function () { 
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+// 7) Scenario: En el que la interfaz es difícil de usar para el usuario # features\historial_de_viajes.feature:34
+//    ? Given el usuario accede al historial de viajes en la aplicación
+//        Undefined. Implement with the following snippet:
+
+//    ? When intenta encontrar información en la interfaz del historial
+//        Undefined. Implement with the following snippet:
+
+         When('intenta encontrar información en la interfaz del historial', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? Then encuentra dificultades para navegar y localizar la información deseada
+//        Undefined. Implement with the following snippet:
+
+         Then('encuentra dificultades para navegar y localizar la información deseada', function () {       
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+// 8) Scenario: En el que ningún viaje debe ser actualizado # features\historial_de_viajes.feature:39
+//    ? Given el usuario accede al historial de viajes en la aplicación
+//        Undefined. Implement with the following snippet:
+
+//    ? When revisa los viajes en el historial
+//        Undefined. Implement with the following snippet:
+
+         When('revisa los viajes en el historial', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+//    ? Then ninguno de los viajes muestra actualizaciones o cambios
+//        Undefined. Implement with the following snippet:
+
+         Then('ninguno de los viajes muestra actualizaciones o cambios', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+// 9) Scenario: En el que algún viaje debe ser actualizado en el historial # features\historial_de_viajes.feature:44
+//    ? Given el usuario accede al historial de viajes en la aplicación
+//        Undefined. Implement with the following snippet:
+
+//    ? When revisa los viajes en el historial
+//        Undefined. Implement with the following snippet:
+
+//    ? Then encuentra al menos un viaje que ha sido actualizado con información nueva o modificada
+//        Undefined. Implement with the following snippet:
+
+         Then('encuentra al menos un viaje que ha sido actualizado con información nueva o modificada', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
